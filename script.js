825c9d6e15db561f073d943457529a8af92e5c2a
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2022-04-22 14:04:03 -0400

    Criado funcoes do tecaldo dos numero e funcao de inicio da votagem na maquina eleitoral

--- script.js
+++ script.js
@@ -4,3 +4,56 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let nuemros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHTML = '';
+
+    for(let i=0;i<etapa.numeros;i++) {
+        if(i == 0) {
+            numeroHTML += '<div class="numero pisca"></div>';
+        } else {
+            numeroHTML += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    nuemros.innerHTML = numeroHTML;
+}
+function atualizaInterface() {
+    alert("Finalizou de digitar o voto!");
+}
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
+    }
+}
+function branco() {
+    alert("Clicou em Branco");
+}
+function corrige() {
+    alert("Clicou em Corrige");
+}
+function confirma() {
+    alert("Clicou em Confirma");
+}
+
+comecarEtapa();
